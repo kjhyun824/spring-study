cedd630cc495617ecd86877fc1b8c531bdc0d710
Junghyun <woori4829@gmail.com>
2021-04-17 20:04:12 +0900

    Practice initialize

---
+++ jh_practice/src/main/java/com/example/jh_practice/JhPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jh_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhPracticeApplication.class, args);
+    }
+
+}
---
+++ jh_practice/src/test/java/com/example/jh_practice/JhPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jh_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
